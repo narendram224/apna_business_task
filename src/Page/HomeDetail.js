fab7ee9a80961d28616aa64bfb7d4333206c7160
narendram224 <narendram224.nm@gmail.com>
2021-08-09 15:13:37 +0530

    Finally done the project & pushed code into git.

--- src/Page/HomeDetail.js
+++ src/Page/HomeDetail.js
@@ -1,26 +1,94 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchCommentDetail } from '../redux/search/searchAction';
+import React,{ useEffect }  from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import CardComponent from "../components/atoms/CardComponent";
+import CommentCard from "../components/CommentCard";
+import ListContent from "../components/molecules/ListContent";
+import Skelton from "../components/Skelton";
+import { isoDateConverter } from "../Helper/IsoDateConverter";
+import { fetchCommentDetail } from "../redux/search/searchAction";
 
 const HomeDetail = () => {
-    let { id } = useParams();
-    const dispatch = useDispatch()
+  let { id } = useParams();
+  const dispatch = useDispatch();
 
-    const initialstate = useSelector((state)=>state.search)
-    const comments =initialstate.commentsDetails;
-console.log("commetn detail",comments);
-    useEffect(() => {
+  const initialstate = useSelector((state) => state.search);
+  const comments = initialstate.commentsDetails; //get detail comment data
+  const loading = initialstate.loading.comment; //get loading data while comment fetch
+  const error = initialstate.error.comment; //get error data while comment fetch
+// call api for comment data if id change 
+  useEffect(() => {
+    dispatch(fetchCommentDetail(id));
+  }, [id]);
+  return (
+    <HomeDetailWrapper>
+      {loading && (
+        <SkeltonWrapper>
+          <Skelton />
+          <Skelton />
+          <Skelton />
+          <Skelton />
+          <Skelton />
+        </SkeltonWrapper>
+      )}
+      {!loading && error.length === 0 && (
+        <CardComponent>
+          <ListContent
+            title={comments["title"]}
+            body={comments["author"]}
+            subtitle={isoDateConverter(comments["created_at"])}
+            icon={false}
+            point={comments["points"]}
+          />
+          <ReplyWrapper>
+            Reply {comments.children && comments.children.length}
+          </ReplyWrapper>
+          {comments["children"] && comments["children"].length > 0 ? (
+            <>
+              {comments.children.map((comment) => {
+                return (
+                  <CommentCard
+                    key={comment["id"]}
+                    title={comment["author"]}
+                    body={comment["text"]}
+                    date={isoDateConverter(comment["created_at"])}
+                  />
+                );
+              })}
+            </>
+          ) : (
+            <ListContent title={"No comments found"} icon={false} />
+          )}
+        </CardComponent>
+      )}
+      {error.length > 0 && (
+        <ListContent title={"Got error while fetching the data"} icon={false} />
+      )}
+    </HomeDetailWrapper>
+  );
+};
+const ReplyWrapper = styled.div`
+  margin: 0;
+  margin-top: 2rem;
+  border-bottom: 2px solid var(--primary-blue);
+  width: 105px;
+  padding-bottom: 0.3rem;
+  font-size: 1.4rem;
+  font-weight: 700;
+  
+`;
 
-        dispatch(fetchCommentDetail(id))
-        
-    }, [id])
-    return (
-        <div>
-            home detail Page
-        </div>
-    )
-}
-
-export default HomeDetail
+const HomeDetailWrapper = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const SkeltonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+`;
+export default HomeDetail;
